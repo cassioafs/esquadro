3f9cfc04bcc4b313a1732e5f40644a8ed52f8916
Cássio Alves <cassio.alves@concrete.com.br>
2017-10-10 13:30:51 -0300

    bug fixes

--- page.js
+++ page.js
@@ -3,48 +3,47 @@
 const EC = protractor.ExpectedConditions;
 const TIME_OUT_DEFAULT = 5000;
 const PATH_DEFAULT_SCREENSHOT = './features/screenshots';
-class Page{
 
-  setValue(element, value, timeout = TIME_OUT_DEFAULT){
+  exports.setValue = (element, value, timeout = TIME_OUT_DEFAULT)=> {
     this.waitForElementToBeVisible(element,timeout);
     element.sendKeys(value);
   }
 
-  clickOn(element, timeout = TIME_OUT_DEFAULT){
+  exports.clickOn = (element, timeout = TIME_OUT_DEFAULT)=> {
     this.waitForElementToBeClickable(element, timeout);
     element.click();
   }
 
-  waitForElementPresenceInDOM(element, timeout = TIME_OUT_DEFAULT){
+  exports.waitForElementPresenceInDOM = (element, timeout = TIME_OUT_DEFAULT)=> {
     browser.wait(EC.presenceOf(element), timeout);
   }
 
-  waitForElementNotPresenceInDOM(element, timeout = TIME_OUT_DEFAULT){
+  exports.waitForElementNotPresenceInDOM = (element, timeout = TIME_OUT_DEFAULT)=> {
     browser.wait(EC.stalenessOf(element), timeout);
   }
 
-  waitForElementToBeClickable(element, timeout = TIME_OUT_DEFAULT){
+  exports.waitForElementToBeClickable = (element, timeout = TIME_OUT_DEFAULT)=> {
     browser.wait(EC.elementToBeClickable(element), timeout);
   }
-  waitForElementToBeVisible(element, timeout = TIME_OUT_DEFAULT){
+  exports.waitForElementToBeVisible = (element, timeout = TIME_OUT_DEFAULT)=> {
     browser.wait(EC.visibilityOf(element), timeout);
   }
 
-  waitForElementToBeInvisible(element, timeout = TIME_OUT_DEFAULT){
+  exports.waitForElementToBeInvisible = (element, timeout = TIME_OUT_DEFAULT)=> {
     browser.wait(EC.invisibilityOf(element), timeout);
   }
 
-  waitAlterUrl(url, timeout = TIME_OUT_DEFAULT){
+  exports.waitAlterUrl = (url, timeout = TIME_OUT_DEFAULT)=> {
     browser.wait(EC.urlContains(url), timeout);
   }
 
-  waitForCounter(elementArrayFinder, expectedCount) {
+  exports.waitForCounter = (elementArrayFinder, expectedCount)=> {
     return elementArrayFinder.count().then(function (actualCount) {
       return expectedCount === actualCount
     })
   }
 
-  swicthDriverToOpenTab(urlNewTab){
+  exports.swicthDriverToOpenTab = (urlNewTab)=> {
     browser.getAllWindowHandles().then(function(handles){
         browser.switchTo().window(handles[1]).then(function(){
           this.waitAlterUrl(urlNewTab);
@@ -52,21 +51,21 @@ class Page{
     });
   }
 
-  returnToMainTab(){
+  exports.returnToMainTab = ()=> {
     browser.getAllWindowHandles().then(function(handles){
         browser.close();
         browser.switchTo().window(handles[0]);
     });
   }
 
-  scrollPageTo(valuePixels){
+  exports.scrollPageTo = (valuePixels)=> {
     browser.executeScript('window.scrollBy(0,' + valuePixels + ');')
   }
-  scrollTop(){
+  exports.scrollTop = ()=> {
     browser.executeScript('window.scrollTo(0,0);');
   }
 
-  getScreenshot(nomeArquivo, pathScreeShot = PATH_DEFAULT_SCREENSHOT) {
+  exports.getScreenshot = (nomeArquivo, pathScreeShot = PATH_DEFAULT_SCREENSHOT)=> {
     function writeScreenShot (data, filename) {
       var stream = fs.createWriteStream(filename);
         stream.write(Buffer.from(data, 'base64'));
@@ -76,5 +75,3 @@ class Page{
       writeScreenShot(png, `${pathScreeShot}/${nomeArquivo}.png`);
     });
   }
-}
-module.exports = Page;
